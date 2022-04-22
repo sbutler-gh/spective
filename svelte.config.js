dd7dd404d8c1fd08b4ee5f663f857bd2f63bbd33
sbutler-gh <sam@ysli.be>
2022-04-22 15:28:38 -0400

    adding netlify config for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
 import cloudflare from "@sveltejs/adapter-cloudflare";
+import netlify from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: cloudflare(),
+    adapter: netlify(),
     // target: '#svelte'
+    			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
   },
 }
 
