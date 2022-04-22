e2f792c9f26cae1ddb7554b8bab6f38f243bf04c
sbutler-gh <sam@ysli.be>
2022-04-22 15:37:16 -0400

    Update netlify config

--- svelte.config.js
+++ svelte.config.js
@@ -5,15 +5,6 @@ import netlify from "@sveltejs/adapter-netlify";
 const config = {
   kit: {
     adapter: netlify(),
-    // target: '#svelte'
-    			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
   },
 }
 
