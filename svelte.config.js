63bcf29a75f8f4d776a5f8d0eaaef64dfb9f4cc8
sbutler-gh <sam@ysli.be>
2022-04-23 12:08:14 -0400

    Updated config and adapter-auto for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import cloudflare from "@sveltejs/adapter-cloudflare";
 import netlify from "@sveltejs/adapter-netlify";
+import auto from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: netlify(),
+    adapter: auto(),
   },
 }
 
